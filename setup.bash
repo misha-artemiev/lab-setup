8a3e65c1af19cc7e71ca1e01168ae861948f5373
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 14:18:35 +0000

    auto-snapshot:  {1 file changed, 4 insertions(+), 1 deletion(-)}

--- setup.bash
+++ setup.bash
@@ -6,4 +6,7 @@ export PATH="$SCRIPT_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$SCRIPT_DIR/lib:$LD_LIBRARY_PATH"
 export XDG_DATA_DIRS="$SCRIPT_DIR/share:$XDG_DATA_DIRS"
 
-alias nvim=XDG_CONFIG_HOME="$SCRIPT_DIR/config" nvim
+nvim_portable() {
+    XDG_CONFIG_HOME="$SCRIPT_DIR/config" nvim "$@"
+}
+alias nvim=nvim_portable
