96652938ee132e6684989ee82e780165ff47a008
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 14:07:31 +0000

    auto-snapshot:  {3 files changed, 30 insertions(+)}
      - create mode 100644 config/nvim/init.lua
      - create mode 100644 config/nvim/lua/config/lazy.lua

--- setup.bash
+++ setup.bash
@@ -5,3 +5,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH="$SCRIPT_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$SCRIPT_DIR/lib:$LD_LIBRARY_PATH"
 export XDG_DATA_DIRS="$SCRIPT_DIR/share:$XDG_DATA_DIRS"
+
+ln -s "$SCRIPT_DIR/config/nvim" ~/.config/nvim
