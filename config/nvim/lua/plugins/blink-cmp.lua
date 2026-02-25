552f334811f034ed855a4e5abe330b7871394362
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 14:10:06 +0000

    auto-snapshot:  {3 files changed, 106 insertions(+)}
      - create mode 100644 config/nvim/lua/plugins/blink-cmp.lua
      - create mode 100644 config/nvim/lua/plugins/catppuccin.lua
      - create mode 100644 config/nvim/lua/plugins/dashboard.lua

---
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,44 @@
+
+local cmp_default = {"lsp", "path", "snippets", "buffer"}
+local cmp_providers = {}
+
+ if vim.g.plugin_lazydev then
+    table.insert(cmp_default, "lazydev")
+    cmp_providers.lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        score_offset = 100,
+    }
+end
+
+return {
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "1.*",
+    opts = {
+        keymap = { preset = "default" },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+        sources = {
+            default = cmp_default,
+            providers = cmp_providers,
+        },
+        completion = {
+            list = { selection = { preselect = false, auto_insert = false } },
+            documentation = { auto_show = false },
+            ghost_text = { enabled = true },
+            menu = {
+                auto_show = true,
+                draw = {
+                    columns = {
+                        { "label", "label_description", gap = 1 },
+                        { "kind_icon", "kind" },
+                    },
+                }
+            },
+        },
+        signature = { enabled = true },
+    },
+    enabled = true
+}
---
+++ config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,53 @@
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function()
+        require("dashboard").setup{
+            theme = "doom",
+            config = {
+                center = {
+                    {
+                        icon = "󰈞 ",
+                        desc = "Find File",
+                        key = "f",
+                        key_hl = "Number",
+                        action = function()
+                            if vim.g.extension_fzf_telescope then
+                                require("telescope.builtin").find_files()
+                            else
+                                require('fzf-lua').files()
+                            end
+                        end,
+                    },
+                    {
+                        icon = " ",
+                        desc = "Open Config",
+                        key = "c",
+                        action = ":Oil ~/.config/nvim"
+                    },
+                    {
+                        icon = "󰒲 ",
+                        desc = "Open lazy",
+                        key = "l",
+                        action = ":Lazy"
+                    },
+                    {
+                        icon = "󰚰 ",
+                        desc = "Lazy Update",
+                        key = "u",
+                        action = ":Lazy update"
+                    },
+                    {
+                        icon = " ",
+                        desc = "Quit",
+                        key = "q",
+                        action = ":qa"
+                    },
+                },
+                vertical_center = true,
+            }
+        }
+    end,
+    enabled = true
+}
