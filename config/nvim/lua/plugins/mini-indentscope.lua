d87e625845c518812c60e0fc721df405f34b9400
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 14:50:15 +0000

    auto-snapshot:  {2 files changed, 42 insertions(+)}
      - create mode 100644 config/nvim/lua/plugins/mini-indentscope.lua
      - create mode 100644 config/nvim/lua/plugins/stay-centered.lua

---
+++ config/nvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,36 @@
+return {
+    "echasnovski/mini.indentscope",
+    lazy = false,
+    config = function()
+        require("mini.indentscope").setup {
+            draw = {
+                delay = 50,
+                animation = require('mini.indentscope').gen_animation.none(),
+            },
+            options = {
+                indent_at_cursor = true,
+            },
+            symbol = "│",
+        }
+
+        -- disable for specific patterns
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "Trouble",
+                "dashboard",
+                "help",
+                "lazy",
+                "mason",
+                "NvimTree",
+                "toggleterm",
+                "trouble",
+                "Outline",
+                "asm", "nasm",
+            },
+            callback = function()
+                vim.b.miniindentscope_disable = true
+            end,
+        })
+    end,
+    enabled = true
+}
