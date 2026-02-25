8f8be42163325a7bde77ec1b03493544bfb0ffca
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 15:22:48 +0000

    auto-snapshot:  {1 file changed, 13 insertions(+)}
      - create mode 100644 config/nvim/lua/plugins/which-key.lua

---
+++ config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/which-key.nvim",
+    lazy = false,
+    opts = {
+        preset = "modern",
+        delay = 1000,
+    },
+    keys = {
+        {"<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)"},
+    },
+    enabled = true
+}
+
