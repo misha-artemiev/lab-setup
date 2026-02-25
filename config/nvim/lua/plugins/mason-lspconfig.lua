c239a1ee226bc706c427ff43b0148c94035b4551
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 14:46:07 +0000

    auto-snapshot:  {1 file changed, 4 insertions(+), 1 deletion(-)}

--- config/nvim/lua/plugins/mason-lspconfig.lua
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,11 @@
+local lsp_servers = {}
+table.insert(lsp_servers, "clangd")
+
 return {
     "mason-org/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-        ensure_installed = {"clangd"},
+        ensure_installed = lsp_servers,
     },
     enabled = true
 }
