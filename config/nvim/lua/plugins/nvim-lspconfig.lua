f72fd1df70730593c388d5d807170713844cd36f
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-02-25 15:21:02 +0000

    auto-snapshot:  {1 file changed, 1 deletion(-)}

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,7 +11,6 @@ return {
       { "<leader>cd", vim.diagnostic.open_float, desc = "LSP: Line Diagnostics" },
       { "gd", vim.lsp.buf.definition, desc = "LSP: Go to Definition" },
       { "K", vim.lsp.buf.hover, desc = "LSP: Hover Documentation" },
-      { "<leader>cA", vim.diagnostic.setqflist, desc = "LSP: Open All Fixes in Quickfix List" },
     },
     enabled = true
 }
